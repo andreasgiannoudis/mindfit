7fce2b7a3a114e5db565123241aac3e1506034bd
andreasgiannoudis <agiannoudis@hotmail.com>
2025-04-10 07:58:15 +0200

    hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import mindfitlogo from '/src/assets/images/mind-fit-logo.png';
-
 const Header = () => {
-  const [show, setShow] = useState(true);
-  let lastScrollY = 0;
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  const handleScroll = () => {
-    if (window.scrollY > lastScrollY) {
-      setShow(false); // scrolling down
-    } else {
-      setShow(true); // scrolling up
-    }
-    lastScrollY = window.scrollY;
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
-    <header className={`header ${!show ? 'header--hidden' : ''}`}>
+    <header className={`header`}>
       <div className="header__container">
         <div className="header__logo">
-          <img src={mindfitlogo} alt="Mind Fit Logo" />
+          <img src={mindfitlogo} alt="Logo" />
         </div>
-        <nav className="header__nav">
-          <a href="#features" className="header__link">Features</a>
+
+        <nav className={`header__nav ${menuOpen ? 'header__nav--open' : ''}`}>
+        <a href="#features" className="header__link">Features</a>
           <a href="#pricing" className="header__link">Pricing</a>
           <a href="#how-it-works" className="header__link">How It Works</a>
           <a href="#testimonials" className="header__link">Testimonials</a>
           <a href="#newsletter" className="header__link header__link--cta">Join Waitlist</a>
         </nav>
+
+        <button
+          className={`header__hamburger ${menuOpen ? 'open' : ''}`}
+          onClick={toggleMenu}
+          aria-label="Toggle navigation"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
       </div>
     </header>
   );
